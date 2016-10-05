7a3981354e4939e369abc29f9c0b09c3bcbefa59
Gary Gutman <mrgaryg68@gmail.com>
2016-10-05 00:37:37 -0400

    Updated to use yum instead of apt-get Added Kafka role

---
+++ ansible_devbox/centos/7/README.md
@@ -0,0 +1,11 @@
+## Episode #45 - Learning Ansible with Vagrant (Part 2/4)
+
+Code from
+* [Sysadmincasts Episode #45 - Learning Ansible with Vagrant (Part 2/4)](https://sysadmincasts.com/episodes/45-learning-ansible-with-vagrant-part-2-4). This was primarily used as a guide line for setting up multiple machines using vagrant. It covers 
+** setup of ssh keys between multiple machines,
+** setup of Ansible managment node 'mgmt'
+** setup of Zookeeper nodes 'zk'
+** setup of Kafaka broker nodes 'broker#'
+* [Github jeqo/ansible-role-confluent-platform ](https://github.com/jeqo/ansible-role-confluent-platform).
+* [Ansible Galaxy jeqo.confluent-platform](https://galaxy.ansible.com/jeqo/confluent-platform/).
+
---
+++ ansible_devbox/centos/7/bootstrap-mgmt.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html) and
+# https://www.liquidweb.com/kb/how-to-install-ansible-on-centos-7-via-yum/
+rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+yum -y update
+# yum -y upgrade
+yum -y install ansible
+
+# copy examples into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/ansible/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+
+# configure hosts file for our internal network defined by Vagrantfile
+cat >> /etc/hosts <<EOL
+
+# vagrant environment nodes
+10.0.15.10  mgmt
+10.0.15.11  zk
+10.0.15.21  broker1
+10.0.15.22  broker2
+10.0.15.23  broker3
+10.0.15.24  broker4
+10.0.15.25  broker5
+10.0.15.26  broker6
+10.0.15.27  broker7
+10.0.15.28  broker8
+10.0.15.29  broker9
+EOL
