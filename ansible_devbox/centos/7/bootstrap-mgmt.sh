4e38c5625aa01807f7e1c639cb7bb02974b24628
Gary Gutman <mrgaryg68@gmail.com>
2016-10-05 23:44:46 -0400

    updated epel rpm version

--- ansible_devbox/centos/7/bootstrap-mgmt.sh
+++ ansible_devbox/centos/7/bootstrap-mgmt.sh
@@ -2,7 +2,7 @@
 
 # install ansible (http://docs.ansible.com/intro_installation.html) and
 # https://www.liquidweb.com/kb/how-to-install-ansible-on-centos-7-via-yum/
-rpm -iUvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+rpm -iUvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
 yum -y update
 # yum -y upgrade
 yum -y install ansible
